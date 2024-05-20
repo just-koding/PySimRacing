759a41549d7b72b65bd0a878dd457d6561be7a94
garrigueta <ferran.garriga@gmail.com>
2024-05-20 17:20:23 +0200

    add worker definitions

---
+++ src/core/class/worker/base.py
@@ -0,0 +1,45 @@
+""" Base Worker Definitions """
+from threading import Thread
+import time
+
+
+class BaseWorker(Thread):
+    """ BaseWorker Class representing a process called in a new thread """
+
+    def __init__(self, *args, **kwargs):
+        """ __init__ """
+        super().__init__(*args, **kwargs)
+        self.running = False
+
+    def run(self):
+        """ run """
+        print("run")
+
+    def start(self):
+        """ run """
+        # thumbnail will be resolved on second OR newly created below thread
+        thread = Thread(target=self.run)
+        thread.start()
+
+    def stop(self):
+        """ stop """
+        self.running = False
+
+
+class BaseTimerWorker(BaseWorker):
+    """ BaseTimerWorker Class representing a timer called in a new thread """
+
+    def __init__(self, *args, **kwargs):
+        """ __init__ """
+        super().__init__(*args, **kwargs)
+        self.cool_down = 20
+
+    def tick(self):
+        """ tick """
+        print("tick")
+
+    def start(self):
+        """ start """
+        while self.running:
+            self.tick()
+            time.sleep(self.cool_down)
---
+++ src/main.py
@@ -0,0 +1,7 @@
+from worker.timer import CustomTimer
+
+
+if __name__ == '__main__':
+    thread1 = CustomTimer()
+    thread1.start()
+    #web_app.run(debug=True, host='0.0.0.0', port=5000)
