0502ca122185be03231dfb34a5faf243125b7b81
garrigueta <ferran.garriga@gmail.com>
2024-05-20 17:27:18 +0200

    add some project files

--- src/ui/main.py
+++ src/ui/main.py
@@ -1,34 +1,40 @@
+""" Main GUI Window """
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 
+
 class Extra(tk.Toplevel):
-	def __init__(self):
-		super().__init__()
-		self.title('extra window')
-		self.geometry('300x400')
-		ttk.Label(self, text = 'A label').pack()
-		ttk.Button(self, text = 'A button').pack()
-		ttk.Label(self, text = 'another label').pack(expand = True)
+    def __init__(self):
+        super().__init__()
+        self.title('extra window')
+        self.geometry('300x400')
+        ttk.Label(self, text = 'A label').pack()
+        ttk.Button(self, text = 'A button').pack()
+        ttk.Label(self, text = 'another label').pack(expand = True)
+
 
 # https://docs.python.org/3/library/tkinter.messagebox.html
 def ask_yes_no():
-	# answer = messagebox.askquestion('Title', 'Body')
-	# print(answer)
-	messagebox.showerror('Info title', 'Here is some information')
+    # answer = messagebox.askquestion('Title', 'Body')
+    # print(answer)
+    messagebox.showerror('Info title', 'Here is some information')
+
 
 def create_window():
-	global extra_window
-	extra_window = Extra()
-	# extra_window = tk.Toplevel()
-	# extra_window.title('extra window')
-	# extra_window.geometry('300x400')
-	# ttk.Label(extra_window, text = 'A label').pack()
-	# ttk.Button(extra_window, text = 'A button').pack()
-	# ttk.Label(extra_window, text = 'another label').pack(expand = True)
+    global extra_window
+    extra_window = Extra()
+    # extra_window = tk.Toplevel()
+    # extra_window.title('extra window')
+    # extra_window.geometry('300x400')
+    # ttk.Label(extra_window, text = 'A label').pack()
+    # ttk.Button(extra_window, text = 'A button').pack()
+    # ttk.Label(extra_window, text = 'another label').pack(expand = True)
+
 
 def close_window():
-	extra_window.destroy()
+    extra_window.destroy()
+
 
 # window
 window = tk.Tk()
